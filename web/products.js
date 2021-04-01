ae9e4358df0cf0cf4a45e30837b9b5d7c7ac2b3a
A2882000 <78910962+A2882000@users.noreply.github.com>
2021-04-01 16:24:58 +0400

    Add files via upload

---
+++ web/products.js
@@ -0,0 +1,106 @@
+let arr = [
+  {
+    "id": 1001,
+    "subject": "English",
+    "description": "This is English book.",
+    "location":"MANCHESTER.",
+    "price": 15.00,  
+    "image": "english1.jpg",
+    "availableInventory": 10,
+  
+  },
+  {
+  
+    "id": 1002,
+    "subject": "Maths",
+    "description": "This is Maths book.",
+    "location": "BRADFORD.",
+    "price": 18.00,
+    "image": "math2.jpg",
+    "availableInventory": 25,
+    "rating": 4
+  },
+  {
+   
+    "id": 1003,
+    "subject": "Physic",
+    "description": "This is Physic book.!",
+    "location": "CAMBRIDGE.",
+    "price": 19,
+    "image": "physic3.jpg",
+    "availableInventory": 7,
+    "rating": 3
+  },
+  {
+    "id": 1004,
+    "subject": "Chemistry",
+    "description": "This is Chemistry book.",
+    "location": "CONTERBURY!",
+    "price": 13.00,
+    "image": "chemistry.png",
+    "availableInventory": 11,
+    "rating": 5
+  },
+  
+  {
+    "id": 1005,
+    "subject": "Biology",
+    "description": "This is Biology book!",
+    "location": "CHESTER.",
+    "price": 20.00,
+    "image": "bio.jpg",
+    "availableInventory": 25,
+    "rating": 4
+  },
+  {
+
+      "id": 1006,
+      "subject": "Animal husbandry",
+      "description": "This is  Animal husbandry book",
+      "location":"GLASGOW",
+      "price": 9.90,
+      "image": "animalhusbandry20.jpg",
+      "availableInventory": 12,
+      "rating": 5
+    },
+    {
+      "id": 1007,
+      "subject": "Civic Education",
+      "description": "This is  Civic book",
+      "location": "LIVERPOOL",
+      "price": 19.00,
+      "image": "civiceducation19.jpg",
+      "availableInventory": 10,
+      "rating": 5
+    },
+    {
+      "id": 1008,
+      "subject": "Arabic",
+      "description": "This is  Arabic  book",
+      "location": "BIRMINGHAM",
+      "price": 18.00,
+      "image": "arab.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    {
+      "id": 1009,
+      "subject": "Literature",
+      "description": "This is  Literature book!",
+      "location": "OXFORD",
+      "price": 16.00,
+      "image": "literature17.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    {
+      "id": 1010,
+      "subject": "Computer",
+      "description": "This is  Computer book.!",
+      "location": "SOUTHAMPTON",
+      "price": 15.00,
+      "image": "computer.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    }
+]
